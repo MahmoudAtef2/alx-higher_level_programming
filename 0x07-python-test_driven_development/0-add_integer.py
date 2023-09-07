1e72adfe19260ebc5e6f9f64c2870447816ccd04
MahmoudAtef2 <matef3431@gmail.com>
2023-09-07 17:34:47 +0000

    first task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function make addition of two numbers."""
+
+def add_integer(a, b=98):
+    """Adds two numbers a and b.
+    
+    Args:
+        a: the first number.
+        b: the second number, default 98.
+
+    Raises:
+        TypeError: If a or b is a non-integer and non-float.
+
+    Returns:
+        The addition of two numbers a and b.
+    """
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
