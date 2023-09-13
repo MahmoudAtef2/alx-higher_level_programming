22cb298b6e78ebcf0a038872b3038d68dbb2c6ff
MahmoudAtef2 <matef3431@gmail.com>
2023-09-13 19:27:12 +0000

    10 & 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Model Square calss'''
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    '''subclass of rectangle class'''
+
+    def __init__(self, size):
+        '''constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Method that return the area of square'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''str reoresentation of square'''
+        return f"[Square] {str(self.__size)}/{str(self.__size)}"
