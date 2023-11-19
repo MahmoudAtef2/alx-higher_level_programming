954ba8f2583a4c6c7f8d43fd090bb5068e0e9a2a
MahmoudAtef2 <matef3431@gmail.com>
2023-11-19 21:07:05 +0000

    .

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
 """
-import sys
+script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
+from sys import argv
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+if __name__ == '__main__':
+    str_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(str_db.format(argv[1], argv[2], argv[3]))
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
+
+    states_del = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_del:
+        session.delete(state)
     session.commit()
+    session.close()
